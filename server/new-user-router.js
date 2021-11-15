a8bc690f2635252d3dfe7f801a51490947f6e2b9
Ido Strauss <strauss02@hotmail.co.il>
2021-11-15 13:46:40 +0200

    applicate new user creation

--- server/new-user-router.js
+++ server/new-user-router.js
@@ -1,10 +1,28 @@
+require('dotenv').config()
+
 const express = require('express')
 const router = express.Router()
+const User = require('./models/user')
 
 router.post('/', (req, res) => {
-  const { username, password } = req.body
-  console.log(username, password)
-  res.send('aloha!')
+  const reqUsername = req.body.username
+  const reqPassword = req.body.password
+  console.log(reqUsername, reqPassword)
+
+  const user = new User({
+    username: reqUsername,
+    password: reqPassword,
+  })
+  console.log('New user created! Here it is:', user)
+
+  user
+    .save()
+    .then((savedUser) => {
+      res.send('User saved!')
+    })
+    .catch((_err) => {
+      res.end('There was an error while trying to save!')
+    })
 })
 
 module.exports = router
